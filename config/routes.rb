6aaf58a9905962bb1903b0623cdc6761d9c8e12b
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-08-21 16:08:25 -0700

    created routes for all controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/index'
-  get 'sessions/new'
-  get 'sessions/create'
+  resources :users
+  resources :laboratories
+  resources :appointments
+  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
