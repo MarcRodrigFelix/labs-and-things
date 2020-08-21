15d8c7fe1c760027e785793cea21e35d964855d3
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-08-21 16:15:21 -0700

    added email uniqueness validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true
+  validates :email, uniqueness: true
   validates :password, length: { in: 6..20 }
   validates :password_confirmation, presence: true
 
