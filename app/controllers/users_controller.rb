f7a9ea19abdc602b475bc4f369a93314ef621ada
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-08-21 18:24:07 -0700

    created new user form and controller params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,21 @@ class UsersController < ApplicationController
   end
 
   def create
- 
+    byebug
+    @user = User.create(user_params)
+    if @user.valid?
+      session[:user_id] = @user.id
+      redirect_to appointments_path
+    else
+      flash[:errors] = ("Sorry, an error occured. Try again.")
+      redirect_to new_user_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :date_of_birth, :password, :password_confirmation)
   end
 
 end
