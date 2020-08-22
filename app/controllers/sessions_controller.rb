74127f970137fa42d1379df0d3e08714daa8c478
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-08-21 17:49:11 -0700

    created sessions create action and destroy, and login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,23 @@
 class SessionsController < ApplicationController
 
   def new
+
   end
 
   def create
+    @user = User.find_by(email: params[:email].try(:authenticate, params[:password]))
+    if @user
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:errors] = ["Oops you have entered an incorrect Email or Password"]
+      redirect_to login_path
+    end
   end
 
   def destroy
+    session.delete :user_id
+    redirect_to login_path
   end
 
 end
