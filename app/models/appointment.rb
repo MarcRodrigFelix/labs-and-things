9fa59cf83ff6ef1b895da50069185ac4b30ec895
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-08-22 18:17:38 -0700

    adds lab attributes method to appointment model to use in appt form for new

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,7 +4,7 @@ class Appointment < ApplicationRecord
 
   def laboratory_attributes=(laboratory_attributes)
     if !laboratory_attributes[:name].empty?
-      laboratory = Laboratory.find_or_create_by(name: laboratory_attributes[:name])
+      laboratory = Laboratory.find_or_create_by(name: laboratory_attributes[:name],lab_type: laboratory_attributes[:lab_type], phone_number: laboratory_attributes[:phone_number], address: laboratory_attributes[:address], lab_hours: laboratory_attributes[:lab_hours], days_of_operation: laboratory_attributes[:days_of_operation])
       laboratory.appointments << self
     end
   end
