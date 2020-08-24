44aab52dd319c5903d8f1826ce29a12dfc458fe6
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-08-23 21:59:31 -0700

    validates all laboratory attributes

--- app/models/laboratory.rb
+++ app/models/laboratory.rb
@@ -1,4 +1,5 @@
 class Laboratory < ApplicationRecord
+  validates :name, :lab_type, :phone_number, :address, :lab_hours, :days_of_operation, presence: true
   has_many :appointments
   has_many :users, through: :appointments
   accepts_nested_attributes_for :appointments, :allow_destroy => true
